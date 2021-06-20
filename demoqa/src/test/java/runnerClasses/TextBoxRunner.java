404c67c713649598f812496bfc6603d8563d76fb
rajat <rajat@LAPTOP-FJ3K051I>
2021-06-20 12:18:44 +0530

    Corrections

--- demoqa/src/test/java/runnerClasses/TextBoxRunner.java
+++ demoqa/src/test/java/runnerClasses/TextBoxRunner.java
@@ -10,10 +10,11 @@ public class TextBoxRunner {
 	
 	WebDriver driver;
 	String url = "https://demoqa.com/text-box";
-	TextBoxPage textBoxPage = PageFactory.initElements(driver, TextBoxPage.class);
+	TextBoxPage textBoxPage;
 	
 	@Test
 	public void test1() {
+		textBoxPage = PageFactory.initElements(driver, TextBoxPage.class);
 		driver = SeleniumInit.getDriver();
 		textBoxPage.navigate(url);
 		textBoxPage.enterDataAndSubmit("Rajat", "rajat@rajat.com", "cA", "pA");
