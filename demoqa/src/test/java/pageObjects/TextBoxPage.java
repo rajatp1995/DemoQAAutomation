6b83a75c62c18b4aa306cfc953de7d8dacaad20a
rajat <rajat@LAPTOP-FJ3K051I>
2021-06-20 12:12:41 +0530

    Added the First Test

---
+++ demoqa/src/test/java/pageObjects/TextBoxPage.java
@@ -0,0 +1,40 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TextBoxPage {
+	final WebDriver driver;
+	 
+	public TextBoxPage(WebDriver driver){
+		this.driver=driver;
+	}
+	
+	public void navigate(String url) {
+		driver.get("https://demoqa.com/text-box");
+	}
+	
+	@FindBy(xpath="//input[@id='userName']")
+	WebElement userName;
+	
+	@FindBy(xpath="//input[@id='userEmail']")
+	WebElement userEmail;
+	
+	@FindBy(xpath="//textarea[@id='currentAddress']")
+	WebElement currentAddress;
+	
+	@FindBy(xpath="//textarea[@id='permanentAddress']")
+	WebElement permanentAddress;
+	
+	@FindBy(xpath="//button[@id='submit']")
+	WebElement submitButton;
+	
+	public void enterDataAndSubmit(String uName, String uEmail, String cAddress, String pAddress) {
+		userName.sendKeys(uName);
+		userEmail.sendKeys(uEmail);
+		currentAddress.sendKeys(cAddress);
+		permanentAddress.sendKeys(pAddress);
+		submitButton.click();
+	}
+}
---
+++ demoqa/src/test/java/runnerClasses/TextBoxRunner.java
@@ -0,0 +1,20 @@
+package runnerClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pageObjects.TextBoxPage;
+import setup.SeleniumInit;
+
+public class TextBoxRunner {
+	
+	WebDriver driver = SeleniumInit.getDriver();
+	String url = "https://demoqa.com/text-box";
+	TextBoxPage textBoxPage = PageFactory.initElements(driver, TextBoxPage.class);
+	
+	@Test
+	public void test1() {
+		textBoxPage.enterDataAndSubmit("Rajat", "rajat@rajat.com", "cA", "pA");
+	}
+
+}
