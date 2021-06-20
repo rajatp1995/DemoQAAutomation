7090c638474ce293482c67beb8129b84ed01eee4
rajat <rajat@LAPTOP-FJ3K051I>
2021-06-20 11:42:41 +0530

    Added more changes to Setup

--- demoqa/src/test/java/setup/SeleniumInit.java
+++ demoqa/src/test/java/setup/SeleniumInit.java
@@ -1,11 +1,29 @@
 package setup;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class SeleniumInit {
+	private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
 	
-	@Test
+	@BeforeSuite
 	public void setup() {
-		System.out.println("Hello");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajat\\git\\DemoQAAutomation\\demoqa\\src\\test\\resources\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/");
 	}
+	
+    @AfterSuite
+    public void afterSuite() throws InterruptedException {
+    	System.out.println("Results written into TestResult.xlsx file.");
+    	Thread.sleep(2000);
+        driver.quit();
+    }
 }
